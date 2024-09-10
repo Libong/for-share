5eda2d6ee17bb8fe47d0c270de06f37446342ff3
陈璐铭 <chenluming@chenluming-Mac.local>
2024-09-10 10:09:36 +0800

    init

--- src/views/navbar/index.vue
+++ src/views/navbar/index.vue
@@ -1,45 +1,56 @@
 <script lang="ts" setup>
 import feather from 'feather-icons';
-import {onMounted} from "vue";
+import {onMounted, ref} from "vue";
 import router from "@/router";
+
+const navbarEl = ref()
 
 onMounted(() => {
   feather.replace();
 });
 
-function toHome() {
-  router.push("/home")
+function to(path:string) {
+  router.push(path)
 }
 
-function toMine() {
-  router.push("/mine")
+
+const options = [
+  {label:'首页',path:'/home',feathe:'home'},
+  {label:'我的',path:'/mine',feathe:'user'},
+  {label:'分享',path:'/share',feathe:'slack'}
+]
+
+
+function mouseenter(){
+  navbarEl.value.at(-1).classList.add('navbar__transition')
+
 }
 
-function toShare() {
-  router.push("/home")
+function mouseleave(){
+  navbarEl.value.at(-1).classList.remove('navbar__transition')
 }
 </script>
 
 <template>
   <div class="bg-container">
     <nav class="navbar">
-      <ul class="navbar__lu">
-        <li class="navbar__li" @click="toHome">
+      <ul class="navbar__lu" @mouseenter="mouseenter" @mouseleave="mouseleave">
+        <li ref="navbarEl" class="navbar__li" @click="to(item.path)" v-for="item in options" >
           <a class="navbar__link">
-            <i data-feather="home"></i>
-            <span>首页</span>
+            <i :data-feather="item.feathe"></i>
+            <span>{{ item.label }}</span>
           </a>
         </li>
-        <li class="navbar__li" @click="toMine">
-          <a class="navbar__link">
-            <i data-feather="user"></i>
-            <span>我的</span></a>
-        </li>
-        <li class="navbar__li" @click="toHome">
-          <a class="navbar__link">
-            <i data-feather="slack"></i>
-            <span>分享</span></a>
-        </li>
+<!--        <li class="navbar__li" @click="toMine">-->
+<!--          <a class="navbar__link">-->
+<!--            <i data-feather="user"></i>-->
+<!--            <span>我的</span></a>-->
+<!--        </li>-->
+<!--        <li class="navbar__li" @click="toHome">-->
+<!--          <a class="navbar__link">-->
+<!--            <i data-feather="slack"></i>-->
+<!--            <span>分享</span></a>-->
+<!--        </li>-->
       </ul>
     </nav>
     <div class="content">
