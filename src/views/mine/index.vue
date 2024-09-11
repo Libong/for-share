b73dea58f96ffe142856bc20f8daaabc17f9bfac
陈璐铭 <chenluming@chenluming-Mac.local>
2024-09-11 17:28:19 +0800

    init

--- src/views/mine/index.vue
+++ src/views/mine/index.vue
@@ -34,11 +34,11 @@ onMounted(()=>{
   });
 
   document.querySelector('.messages-btn')?.addEventListener('click', function () {
-    document.querySelector('.messages-section').classList.add('show');
+    document.querySelector('.messages-section')?.classList.add('show');
   });
 
   document.querySelector('.messages-close')?.addEventListener('click', function () {
-    document.querySelector('.messages-section').classList.remove('show');
+    document.querySelector('.messages-section')?.classList.remove('show');
   });
 })
 
