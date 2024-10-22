9e1a74a2a4ac0b1c0994e63a6e651f0bb455bc0c
Adam19981 <luming369852@qq.com>
2024-10-22 23:59:15 +0800

    修复butotn无特效

--- src/components/common/button/Button2.vue
+++ src/components/common/button/Button2.vue
@@ -1,56 +1,64 @@
-<script>
+<script setup>
 import gsap from "gsap";
+import {onMounted} from "vue";
 
 gsap.registerPlugin(Physics2DPlugin);
 
-document.querySelectorAll('.button').forEach(button => {
 
-  const bounding = button.getBoundingClientRect()
+onMounted(()=>{
 
-  // button.addEventListener('mousemove', e => {
-  //
-  //   let dy = (e.clientY - bounding.top - bounding.height / 2) / -1
-  //   let dx = (e.clientX - bounding.left - bounding.width / 2) / 10
-  //
-  //   dy = dy > 10 ? 10 : (dy < -10 ? -10 : dy);
-  //   dx = dx > 4 ? 4 : (dx < -4 ? -4 : dx);
-  //
-  //   button.style.setProperty('--rx', dy);
-  //   button.style.setProperty('--ry', dx);
-  //
-  // });
-  //
-  // button.addEventListener('mouseleave', e => {
-  //
-  //   button.style.setProperty('--rx', 0)
-  //   button.style.setProperty('--ry', 0)
-  //
-  // });
+  document.querySelectorAll('#customButton').forEach(button => {
 
-  button.addEventListener('click', e => {
-    button.classList.add('success');
-    gsap.to(button, {
-      '--icon-x': -3,
-      '--icon-y': 3,
-      '--z-before': 0,
-      duration: .2,
-      onComplete() {
-        particles(button.querySelector('.emitter'), 100, -4, 6, -80, -50);
-        gsap.to(button, {
-          '--icon-x': 0,
-          '--icon-y': 0,
-          '--z-before': -6,
-          duration: 1,
-          ease: 'elastic.out(1, .5)',
-          onComplete() {
-            button.classList.remove('success');
-          }
-        });
-      }
+    const bounding = button.getBoundingClientRect()
+
+    // button.addEventListener('mousemove', e => {
+    //
+    //   let dy = (e.clientY - bounding.top - bounding.height / 2) / -1
+    //   let dx = (e.clientX - bounding.left - bounding.width / 2) / 10
+    //
+    //   dy = dy > 10 ? 10 : (dy < -10 ? -10 : dy);
+    //   dx = dx > 4 ? 4 : (dx < -4 ? -4 : dx);
+    //
+    //   button.style.setProperty('--rx', dy);
+    //   button.style.setProperty('--ry', dx);
+    //
+    // });
+    //
+    // button.addEventListener('mouseleave', e => {
+    //
+    //   button.style.setProperty('--rx', 0)
+    //   button.style.setProperty('--ry', 0)
+    //
+    // });
+
+    button.addEventListener('click', e => {
+
+      button.classList.add('success');
+      gsap.to(button, {
+        '--icon-x': -3,
+        '--icon-y': 3,
+        '--z-before': 0,
+        duration: .2,
+        onComplete() {
+          particles(button.querySelector('.emitter'), 100, -4, 6, -80, -50);
+          gsap.to(button, {
+            '--icon-x': 0,
+            '--icon-y': 0,
+            '--z-before': -6,
+            duration: 1,
+            ease: 'elastic.out(1, .5)',
+            onComplete() {
+              button.classList.remove('success');
+            }
+          });
+        }
+      });
     });
+
   });
 
-});
+})
+
 
 function particles(parent, quantity, x, y, minAngle, maxAngle) {
   let colors = [
@@ -97,7 +105,7 @@ function particles(parent, quantity, x, y, minAngle, maxAngle) {
 </script>
 
 <template>
-  <button class="button">
+  <button class="button" id="customButton">
     <div class="icon">
       <div class="cannon"></div>
       <div class="confetti">
