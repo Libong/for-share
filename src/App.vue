326947d75874ef82371d5a8a26730462df40c460
libong <646071005@qq.com>
2024-10-24 00:40:28 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import Button2 from "@/components/common/button/Button2.vue";
+</script>
 
 <template>
   <div>
-    <router-view :key="$route.fullPath"></router-view>
+    <!--    <router-view :key="$route.fullPath"></router-view>-->
+    <Button2></Button2>
   </div>
 </template>
