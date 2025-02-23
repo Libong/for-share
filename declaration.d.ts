741b63d405680b0d6093ec593cb38505184c75ce
libong <646071005@qq.com>
2025-02-23 15:01:28 +0800

    完成记录按钮新增

--- declaration.d.ts
+++ declaration.d.ts
@@ -1,9 +1,13 @@
 /*
-    类型声明文件 防止有些组件没有适配ts
- */
+类型声明文件 防止有些组件没有适配ts
+*/
 declare module "*.mjs";
 declare module '*.vue' {
     import {DefineComponent} from 'vue';
     const component: DefineComponent<{}, {}, any>;
     export default component;
+}
+// src/shims.d.ts
+declare module '@/components/common/button/test.js' {
+    export function clickButton(): void;
 }
--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script lang="ts" setup>
 
+import Test from "@/components/common/button/test.vue";
 </script>
 
 <template>
   <div>
-    <el-config-provider :locale="zhCn">
-      <router-view :key="$route.fullPath"></router-view>
-    </el-config-provider>
+    <test></test>
+    <!--    <el-config-provider :locale="zhCn">-->
+    <!--      <router-view :key="$route.fullPath"></router-view>-->
+    <!--    </el-config-provider>-->
   </div>
 </template>
